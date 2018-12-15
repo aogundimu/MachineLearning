ce54fd8cca1b0037ae6c24db4b68140df8dfb95e
Augustine Ogundimu <ogundimuao@gmail.com>
2018-12-15 05:33:02 -0800

    Second checkin

---
+++ Week 8/machine-learning-ex7/ex7/findClosestCentroids.m
@@ -0,0 +1,46 @@
+function idx = findClosestCentroids(X, centroids)
+%FINDCLOSESTCENTROIDS computes the centroid memberships for every example
+%   idx = FINDCLOSESTCENTROIDS (X, centroids) returns the closest centroids
+%   in idx for a dataset X where each row is a single example. idx = m x 1 
+%   vector of centroid assignments (i.e. each entry in range [1..K])
+%
+
+% Set K
+K = size(centroids, 1);
+
+% You need to return the following variables correctly.
+idx = zeros(size(X,1), 1);
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Go over every example, find its closest centroid, and store
+%               the index inside idx at the appropriate location.
+%               Concretely, idx(i) should contain the index of the centroid
+%               closest to example i. Hence, it should be a value in the 
+%               range 1..K
+%
+% Note: You can use a for-loop over the examples to compute this.
+%
+
+count = size(X,1);
+
+for outer = 1:count,
+    min_distance = intmax()
+    for inner = 1:K,
+       curr_distance = sum( (X(outer,:) - centroids(inner,:) ).^2);
+       if curr_distance < min_distance,
+          min_distance = curr_distance;
+          idx(outer) = inner;
+       end
+    end
+end
+
+
+
+
+
+
+
+% =============================================================
+
+end
+
